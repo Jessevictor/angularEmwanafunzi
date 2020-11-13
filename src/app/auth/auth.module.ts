7456e6e3562fbf73d854f4f8daa9ed4534d8b896
Ronny <mwithiamoki@gmail.com>
2020-11-13 13:50:26 +0300

    created resource

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,19 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ResourcesComponent } from './components/resources/resources.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent],
+  declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent, ResourcesComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
+  ],
+  exports:[
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent
   ]
 })
 export class AuthModule { }
