f5fa9bd7f40058808463cbefec1d67f2c5e25ebe
Ronny <mwithiamoki@gmail.com>
2020-11-17 12:19:35 +0300

    login and register ui

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,11 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { ResourcesComponent } from './components/resources/resources.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent, ResourcesComponent],
+  declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../resources/auth.service';
-import { ProgressbarService } from 'src/app/shared/services/progressbar.service';
-import { AlertService } from 'ngx-alerts';
 
 @Component({
   selector: 'app-login',
@@ -11,32 +7,9 @@ import { AlertService } from 'ngx-alerts';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(
-    private authService: AuthService,
-    private progressService: ProgressbarService,
-    private alertService: AlertService
-  ) { }
+  constructor() { }
 
-  ngOnInit(): void {}
-  onSubmit(f: NgForm) {
-    this.alertService.info('Check login information');
-    this.progressService.startLoading();
-
-    const loginObserver = {
-      next: (x) => {
-        this.progressService.setSuccess();
-        this.alertService.success('Welcome back ' + x.username);
-        this.progressService.completeLoading();
-      },
-      error: (err) => {
-        this.progressService.setFailure();
-        console.log(err);
-        this.alertService.danger('Unable to Login');
-        this.progressService.completeLoading();
-      },
-    };
-
-    this.authService.login(f.value).subscribe(loginObserver);
+  ngOnInit(): void {
   }
 
 }
